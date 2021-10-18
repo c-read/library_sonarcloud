af30dbb670c8cd17bfd66cd05c2490b8f64cf08f
connor read <cread@softwareinstitute.com>
2021-10-18 10:10:19 +0100

    added unit tests

--- src/test/java/com/tsi/training/cread/BookTest.java
+++ src/test/java/com/tsi/training/cread/BookTest.java
@@ -2,6 +2,9 @@ package com.tsi.training.cread;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class BookTest
@@ -9,8 +12,9 @@ public class BookTest
     @Test
     public void testBookloanmethod()
     {
-        Book book = new Book("Enlightenment", 24);
+        Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
         assertEquals("message","Enlightenment",book.getName());
+        assertEquals("message",600,book.getLength());
     }
 
 
@@ -24,7 +28,9 @@ public class BookTest
     @Test
     public void testAudioBookloanmethod()
     {
-        AudioBook audioBook = new AudioBook("Captain Underpants", 4);
+        AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
         assertEquals("message","Captain Underpants",audioBook.getName());
+        assertEquals("message",100,audioBook.getSizeMb());
+
     }
 }
