3d279eb50a6ef0626b2c3cae156df9d07d2ca6ac
connor read <cread@softwareinstitute.com>
2021-10-18 12:45:06 +0100

    add more unit tests

--- src/test/java/com/tsi/training/cread/BookTest.java
+++ src/test/java/com/tsi/training/cread/BookTest.java
@@ -9,18 +9,37 @@ import static org.junit.Assert.assertEquals;
 
 public class BookTest
 {
+
+    @Test
+    public void testBookGetNameMethod()
+    {
+        Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
+        assertEquals("message","Enlightenment",book.getName());
+    }
+    @Test
+    public void testBookGetIdMethod()
+    {
+        Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
+        assertEquals("message",24,book.getId());
+    }
+    @Test
+    public void testBookGetGenreMethod()
+    {
+        Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
+        assertEquals("failed to get genre","non-fiction",book.getGenre());
+    }
+    @Test
+    public void testBookGetLengthMethod()
+    {
+        Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
+        assertEquals("message",600,book.getLength());
+    }
     @Test
     public void testBookloanmethod()
     {
         Book book = new Book("Enlightenment", 24,"non-fiction",new ArrayList<>(List.of("Roy Porter")),600);
-        assertEquals("message","Enlightenment",book.getName());
-        assertEquals("message",24,book.getId());
-        assertEquals("failed to get genre","non-fiction",book.getGenre());
-        assertEquals("message",600,book.getLength());
         assertEquals("message","borrowing book from library",book.loan());
     }
-
-
     @Test
     public void testBookreturnmethod()
     {
@@ -32,19 +51,36 @@ public class BookTest
     public void testAudioBookloanmethod()
     {
         AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
-        assertEquals("message","Captain Underpants",audioBook.getName());
-        assertEquals("message",4,audioBook.getId());
-        assertEquals("failed to get genre","fiction",audioBook.getGenre());
-        assertEquals("message",100,audioBook.getSizeMb());
         assertEquals("message","audiobook has been downloaded",audioBook.loan());
-
     }
-
+    @Test
+    public void testAudioBookGetNameMethod()
+    {
+        AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
+        assertEquals("message","Captain Underpants",audioBook.getName());
+    }
+    @Test
+    public void testAudioBookGetIdMethod()
+    {
+        AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
+        assertEquals("message",4,audioBook.getId());
+    }
+    @Test
+    public void testAudioBookGetGenreMethod()
+    {
+        AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
+        assertEquals("message","fiction",audioBook.getGenre());
+    }
+    @Test
+    public void testAudioBookGetSizeMbMethod()
+    {
+        AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
+        assertEquals("message",100,audioBook.getSizeMb());
+    }
     @Test
     public void testAudioBookreturnmethod()
     {
         AudioBook audioBook = new AudioBook("Captain Underpants", 4,"fiction",new ArrayList<>(List.of("na")),100);
-        assertEquals("message",100,audioBook.getSizeMb());
         assertEquals("message","returning to library",audioBook.returnToLibrary());
 
     }
